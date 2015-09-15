dae4030cad13db1ed7e34f794a6553719bafd430
ZhangZhi1993-2025 <1557983850@qq.com>
2015-09-15 15:12:39 +0800

    update

--- SwordToOffer/07_QueueBasedOnTwoStacks/main.cpp
+++ SwordToOffer/07_QueueBasedOnTwoStacks/main.cpp
@@ -5,28 +5,17 @@ using namespace std;
 class Solution_7 {
 public:
     void push ( int node ) {
-        int tmp = 0;
-        if ( stack2.empty() )
-            stack1.push ( node );
-        else {
-            while ( !stack2.empty() ) {
-                tmp = stack2.top();
-                stack1.push ( tmp );
-                stack2.pop();
-            }
-            stack1.push ( node );
-        }
+        stack1.push ( node );
     }
 
     int pop() {
         int tmp = 0;
-        if ( stack1.empty() ) {
+        if ( !stack2.empty() ) {
             tmp = stack2.top();
             stack2.pop();
         } else {
             while ( stack1.size() > 1 ) {
-                tmp = stack1.top();
-                stack2.push ( tmp );
+                stack2.push ( stack1.top() );
                 stack1.pop();
             }
             tmp = stack1.top();
