651aebe1c969b8584d29d0ce1dac83b7fbc25322
ZhangZhi <1557983850@qq.com>
2015-10-09 00:00:14 +0800

    update

---
+++ CodingInterviewGuide/4LongestCommonSubString/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+string longestSubString ( string a, string b ) {
+    int max = 0;
+    int begin = 0;
+    int end = 0;
+    for ( int i = 0; i < a.size(); i++ ) {
+
+    }
+}
+
+int main () {
+    return 0;
+}
