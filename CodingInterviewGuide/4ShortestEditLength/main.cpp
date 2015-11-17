257d8d8ee33f8a3baf7f84136efa814bf1a2b71d
ZhangZhi <1557983850@qq.com>
2015-11-17 16:49:53 +0800

    update

--- CodingInterviewGuide/4ShortestEditLength/main.cpp
+++ CodingInterviewGuide/4ShortestEditLength/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
 /**
- * @brief dynamic programming with O(n^2) space
+ * @brief change str1 to str2,dynamic programming with O(n^2) space
  * @param ic:insert cost
  * @param dc:delete cost
  * @param rc:replace cost
  */
-int shortestEditLength1 ( string str1, string str2, int ic, int dc, int rc ) {
+vector<string> shortestEditLength1 ( string str1, string str2, int ic, int dc, int rc ) {
     int **dp = new int*[str1.size() + 1];
     for ( unsigned int i = 0; i < str1.size(); i++ )
         dp[i] = new int[str2.size() + 1];
@@ -31,11 +33,42 @@ int shortestEditLength1 ( string str1, string str2, int ic, int dc, int rc ) {
             }
         }
 
-    int result = dp[str1.size() - 1][str2.size() - 1];
+    vector<string> result;
+    stack<string> stk;
+    int i = str1.size() - 1;
+    int j = str2.size() - 1;
+
+    while ( i > 0 || j > 0 ) {
+        if ( i > 0 && j > 0 ) {
+            if ( dp[i][j] + dc == dp[i - 1][j] )
+                stk.push ( "delete " + str1[i--] );
+            else if ( dp[i][j] + ic == dp[i][j - 1] )
+                stk.push ( "insert " + str2[j--] );
+            else if ( dp[i][j] == dp[i - 1][j - 1] ) {
+                stk.push ( "the two strings have the same letter " + str1[i--] );
+                j--;
+            } else if ( dp[i][j] + rc == dp[i - 1][j - 1] ) {
+                string tmp = "replace " + str1[i--];
+                tmp = tmp + " to " + str2[j--];
+                stk.push ( tmp );
+            }
+        } else if ( i == 0 ) {
+            while ( j >= 0 )
+                stk.push ( "insert " + str2[j--] );
+        } else if ( j == 0 ) {
+            while ( i >= 0 )
+                stk.push ( "delete " + str1[i--] );
+        }
+    }
+
+    while ( !stk.empty() ) {
+        result.push_back ( stk.top() );
+        stk.pop();
+    }
 
     for ( unsigned int i = 0; i < str1.size(); i++ )
         delete dp[i];
-    delete[] dp;
+    delete dp;
 
     return result;
 }
@@ -45,10 +78,10 @@ int shortestEditLength2 ( string str1, string str2, int ic, int dc, int rc ) {
     int *dp;
     if ( str1.size() < str2.size() ) {
         dp = new int[str1.size()];
-        for ( int i = 0; i < str1.size(); i++ )
+        for ( unsigned int i = 0; i < str1.size(); i++ )
             dp[i] = i * dc;
 
-        for ( int i = 0; i < str1.size(); i++ ) {
+        for ( unsigned int i = 0; i < str1.size(); i++ ) {
 
         }
 
