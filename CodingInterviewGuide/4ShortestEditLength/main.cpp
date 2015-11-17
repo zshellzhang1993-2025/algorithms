9b6e3f8d456f72ae2ac18e60b434b86f44c56d65
ZhangZhi <1557983850@qq.com>
2015-11-17 22:48:57 +0800

    update

--- CodingInterviewGuide/4ShortestEditLength/main.cpp
+++ CodingInterviewGuide/4ShortestEditLength/main.cpp
@@ -11,33 +11,32 @@ using namespace std;
  */
 vector<string> shortestEditLength1 ( string str1, string str2, int ic, int dc, int rc ) {
     int **dp = new int*[str1.size() + 1];
-    for ( unsigned int i = 0; i < str1.size(); i++ )
+    for ( unsigned int i = 0; i < str1.size() + 1; i++ )
         dp[i] = new int[str2.size() + 1];
 
     //initialize
-    for ( unsigned int i = 0; i < str2.size(); i++ )
+    for ( unsigned int i = 0; i < str2.size() + 1; i++ )
         dp[0][i] = i * ic;
-    for ( unsigned int i = 1; i < str1.size(); i++ )
+    for ( unsigned int i = 1; i < str1.size() + 1; i++ )
         dp[i][0] = i * dc;
 
     //run dynamic programming algorithm
-    for ( unsigned int i = 1; i < str1.size(); i++ )
-        for ( unsigned int j = 1; j < str2.size(); j++ ) {
-            if ( str1[i] == str2[j] )
-                dp[i][j] = dp[i - 1][j - 1];
-            else {
-                dp[i][j] = dp[i - 1][j] + dc < dp[i][j - 1] + ic ?
-                           dp[i - 1][j] + dc : dp[i][j - 1] + ic;
-                if ( dp[i - 1][j - 1] + rc < dp[i][j] )
-                    dp[i][j] = dp[i - 1][j - 1] + rc;
-            }
+    for ( unsigned int i = 1; i < str1.size() + 1; i++ )
+        for ( unsigned int j = 1; j < str2.size() + 1; j++ ) {
+            dp[i][j] = dp[i - 1][j] + dc < dp[i][j - 1] + ic ?
+                       dp[i - 1][j] + dc : dp[i][j - 1] + ic;
+            if ( str1[i] == str2[j] && dp[i - 1][j - 1] < dp[i][j] ) {
+                dp[i][j] = dp[i - 1][j] + dc;
+            } else if ( str1[i] != str2[j] && dp[i - 1][j - 1] + rc < dp[i][j] )
+                dp[i][j] = dp[i - 1][j - 1] + rc;
         }
 
     vector<string> result;
     stack<string> stk;
-    int i = str1.size() - 1;
-    int j = str2.size() - 1;
+    int i = str1.size();
+    int j = str2.size();
 
+    //find from the tail to the head for the path
     while ( i > 0 || j > 0 ) {
         if ( i > 0 && j > 0 ) {
             if ( dp[i][j] + dc == dp[i - 1][j] )
@@ -76,19 +75,60 @@ vector<string> shortestEditLength1 ( string str1, string str2, int ic, int dc, i
 // dynamic programming with condition compress in O(min{str1.size()+1,str2.size()+1}) space
 int shortestEditLength2 ( string str1, string str2, int ic, int dc, int rc ) {
     int *dp;
+    int result = 0;
+    int left_top = 0;
+    int tmp = 0;
     if ( str1.size() < str2.size() ) {
-        dp = new int[str1.size()];
-        for ( unsigned int i = 0; i < str1.size(); i++ )
+        dp = new int[str1.size() + 1];
+        for ( unsigned int i = 0; i < str1.size() + 1; i++ )
             dp[i] = i * dc;
 
-        for ( unsigned int i = 0; i < str1.size(); i++ ) {
+        for ( unsigned int i = 1; i < str2.size() + 1; i++ )
+            for ( unsigned int j = 0; j < str1.size() + 1; j++ ) {
+                tmp = dp[j];
+                if ( j == 0 ) {
+                    dp[j] = dp[j] + ic;
+                } else {
+                    dp[j] = dp[j] + ic < dp[j - 1] + dc ?
+                            dp[j] + ic : dp[j - 1] + dc;
+                    if ( str1[j] == str2[i] && left_top < dp[j] )
+                        dp[j] = left_top;
+                    else if ( str1[j] != str2[i] && left_top + rc < dp[j] )
+                        dp[j] = left_top + rc;
+                }
+                left_top = tmp;
+            }
 
-        }
-
+        result = dp[str1.size()];
         delete dp;
+
+        return result;
+
     } else {
-        dp = new int[str2.size()];
+        dp = new int[str2.size() + 1];
+        for ( unsigned int i = 0; i < str2.size() + 1; i++ )
+            dp[i] = i * ic;
+
+        for ( unsigned int i = 1; i < str1.size() + 1; i++ )
+            for ( unsigned int j = 0; j < str2.size() + 1; j++ ) {
+                tmp = dp[j];
+                if ( j == 0 ) {
+                    dp[j] = dp[j] + dc;
+                } else {
+                    dp[j] = dp[j - 1] + ic < dp[j] + dc ?
+                            dp[j - 1] + ic : dp[j] + dc;
+                    if ( str1[i] == str2[j] && left_top < dp[j] )
+                        dp[j] = left_top;
+                    else if ( str1[i] != str2[j] && left_top + rc < dp[j] )
+                        dp[j] = left_top + rc;
+                }
+                left_top = tmp;
+            }
+
+        result = dp[str1.size()];
         delete dp;
+
+        return result;
     }
 }
 
