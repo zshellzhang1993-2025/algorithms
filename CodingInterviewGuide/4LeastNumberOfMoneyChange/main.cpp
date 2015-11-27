76c019f3d71f1690c2f47ec1599a17e490acbee4
ZhangZhi <1557983850@qq.com>
2015-11-27 19:37:44 +0800

    update

--- CodingInterviewGuide/4LeastNumberOfMoneyChange/main.cpp
+++ CodingInterviewGuide/4LeastNumberOfMoneyChange/main.cpp
@@ -18,12 +18,18 @@ vector<int> leastNumberOfMoneyChange1 ( int array[], int n, int aim ) {
         }
     }
 
+    // if has no result
+    if ( dp[aim] == INT_MAX - 1 ) {
+        vector<int> error ( 1, -1 );
+        return error;
+    }
+
     vector<int> result ( dp[aim] );
     int current = dp[aim] - 1;
     int target = aim;
     while ( target > 0 ) {
         for ( int i = 0; i < n; i++ ) {
-            if ( dp[target] - 1 == dp[target - array[i]] ) {
+            if ( target >= array[i] && dp[target] - 1 == dp[target - array[i]] ) {
                 result[current] = array[i];
                 current--;
                 target = target - array[i];
@@ -57,6 +63,12 @@ vector<int> leastNumberOfMoneyChange2 ( int array[], int n, int aim ) {
                 dp[i][j] = dp[i - 1][j];
         }
 
+    // if has no result
+    if ( dp[n - 1][aim] == INT_MAX - 1 ) {
+        vector<int> error ( 1, -1 );
+        return error;
+    }
+
     vector<int> result ( dp[n - 1][aim] );
     int current = dp[n - 1][aim] - 1;
     int target = aim;
@@ -68,7 +80,8 @@ vector<int> leastNumberOfMoneyChange2 ( int array[], int n, int aim ) {
                 result[current] = array[0];
             break;
         }
-        if ( dp[index][target] - 1 == dp[index - 1][target - array[index]] ) {
+        if ( target >= array[index] && dp[index][target] - 1
+                == dp[index - 1][target - array[index]] ) {
             result[current] = array[index];
             current--;
             target = target - array[index];
@@ -84,53 +97,50 @@ vector<int> leastNumberOfMoneyChange2 ( int array[], int n, int aim ) {
 
 // 0/1 backpack problem with condition compress
 int leastNumberOfMoneyChange3 ( int array[], int n, int aim ) {
-    int **dp = new int*[2];
-    for ( int i = 0; i < 2; i++ )
-        dp[i] = new int[aim + 1];
-    dp[0][0] = 0;
+    int *dp = new int[aim + 1];
+    dp[0] = 0;
     for ( int i = 1; i < aim + 1; i++ )
-        dp[0][i] = INT_MAX - 1;
+        dp[i] = INT_MAX - 1;
     if ( array[0] <= aim )
-        dp[0][array[0]] = 1;
+        dp[array[0]] = 1;
 
     for ( int i = 1; i < n - 1; i++ ) {
-        for ( int j = 0; j < aim + 1; j++ ) {
-            if ( j >= array[i] && dp[0][j - array[i]] + 1 < dp[0][j] )
-                dp[1][j] = dp[0][j - array[i]] + 1;
-            else
-                dp[1][j] = dp[0][j];
+        for ( int j = aim; j > 0 ; j-- ) {
+            if ( j >= array[i] && dp[j - array[i]] + 1 < dp[j] )
+                dp[j] = dp[j - array[i]] + 1;
         }
-        for ( int j = 0; j < aim; j++ )
-            dp[0][j] = dp[1][j];
     }
 
     //the last row does not need to be all-element calculated at all
     //the last element of the last row is enough
     int result = 0;
-    if ( aim >= array[n - 1] && dp[0][aim - array[n - 1]] + 1 < dp[0][aim] )
-        result = dp[0][aim - array[n - 1]] + 1;
+    if ( aim >= array[n - 1] && dp[aim - array[n - 1]] + 1 < dp[aim] )
+        result = dp[aim - array[n - 1]] + 1;
     else
-        result = dp[0][aim];
+        result = dp[aim];
 
-    for ( int i = 0; i < 2; i++ )
-        delete dp[i];
     delete dp;
 
-    return result;
+    return result == INT_MAX - 1 ? -1 : result;
+}
+
+// multi backpack problem
+vector<int> leastNumberOfMoneyChange4 ( int array[], int n, int aim ) {
+
 }
 
 int main () {
     int array1[3] = {5, 2, 3};
-    vector<int> result = leastNumberOfMoneyChange1 ( array1, 3, 10 );
+    vector<int> result = leastNumberOfMoneyChange1 ( array1, 3, 1 );
     for ( vector<int>::iterator iter = result.begin(); iter != result.end(); iter++ )
         cout << *iter << " ";
     cout << endl;
-    int array2[4] = {5, 2, 3, 5};
-    result = leastNumberOfMoneyChange2 ( array2, 4, 10 );
+    int array2[5] = {5, 2, 3, 5};
+    result = leastNumberOfMoneyChange2 ( array2, 4, 1 );
     for ( vector<int>::iterator iter = result.begin(); iter != result.end(); iter++ )
         cout << *iter << " ";
     cout << endl;
-    cout << leastNumberOfMoneyChange3 ( array2, 4, 10 );
+    cout << leastNumberOfMoneyChange3 ( array2, 4, 12 );
     cout << endl;
     return 0;
 }
